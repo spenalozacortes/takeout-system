f7259af89180ba3a7dc27f0ea6469beeafa5aaa8
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-07-26 16:47:36 -0600

    Create getOutputOnIntInput method

--- TakeOutSimulator.java
+++ TakeOutSimulator.java
@@ -12,4 +12,22 @@ public class TakeOutSimulator {
     this.customer = customer;
     this.input = input;
   }
+
+  // Methods
+  private <T> T getOutputOnIntInput(String userInputPrompt, IntUserInputRetriever<T> intUserInputRetriever) {
+    while(true) {
+      System.out.println(userInputPrompt);
+      if (input.hasNextInt()) {
+        int userInput = input.nextInt();
+        try {
+          return intUserInputRetriever.produceOutputOnIntUserInput(userInput);
+        } catch(IllegalArgumentException e) {
+          System.out.println(userInput + " is not a valid input. Try Again!");
+        }
+      } else {
+        System.out.println("Input needs to be an `int` type.");
+        input.next();
+      }
+    }
+  }
 }
